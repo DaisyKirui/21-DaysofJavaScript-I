d39be10f4db575a5b2ff3c230bdfe16a0fbf6d0b
Daisy Kirui <126683404+DaisyJJKirui@users.noreply.github.com>
2024-01-07 21:10:03 +0300

    practicing JavaScript

---
+++ Day04/main.js
@@ -0,0 +1,70 @@
+//How to declare a variable
+
+
+var myAge = 78;
+let myName = 'Daisy';
+const myGender = 'girl';
+
+
+var myAge = 12;
+
+myName = 'Abby';
+
+
+
+console.log(myAge, myName);
+
+//How to declare a variable
+
+let number1 = 5;
+let number2 = 29;
+
+let sum = number1 +number2;
+
+console.log(sum)
+
+let number = 90;//is
+
+// =
+// === or ==
+
+let A = 20;
+let B = 90;
+
+// >, <, <=, >==
+A < B
+
+//!=
+A != B
+
+// Control Structure - Conditional Statement
+
+
+// Control Structure - Conditional Statement (while, switch, if, if...else) - Boolean (true or false)
+
+if (A === B){
+  console.log('A and B are equal')
+} else {
+  console.log("A and B aren't equal")
+}
+
+let weather = 'sunny';
+let time = 'busy'
+
+
+// && - AND
+// || - OR
+// conditional statement - true or
+
+if (weather === 'sunny' || time === 'not busy') {
+ console.log ("You don't have time to swim")
+} else {
+ console.log ('Go swim')
+}
+
+let num1 = 10
+let num2 = 2
+
+let division = num1 / num2
+
+console.log (division)
